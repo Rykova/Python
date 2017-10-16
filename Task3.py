93f8cba66afcd725eeb7ace693ae54e0f4caec8d
Rykova <32102625+Rykova@users.noreply.github.com>
2017-10-16 17:29:53 +0300

    Update Task3.py
    New version

--- Task3.py
+++ Task3.py
@@ -2,19 +2,22 @@ import sys
 import matplotlib
 import numpy as np
 
-
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
 from matplotlib.patches import Circle
 
+from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 from tkinter.colorchooser import *
 from tkinter import filedialog
 
+import pickle
+entry = {}
 
 LARGE_FONT = ("Verdana", 12)
+
 
 def isConvertibleToFloat(value):
     try:
@@ -22,6 +25,16 @@ def isConvertibleToFloat(value):
         return True
     except:
         return False
+
+def file_save():
+    f = filedialog.asksaveasfile(mode='w', defaultextension=".xml")
+    if f is None:  # asksaveasfile return `None` if dialog closed with "cancel".
+        return
+    f.close()  # `()` was missing.
+    with open(f.name, 'wb') as handle:
+        pickle.dump(entry, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
 
 class SeaofBTCapp(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -50,25 +63,31 @@ class SeaofBTCapp(tk.Tk):
         frame.tkraise()
 
 class GraphPage(tk.Frame):
+
+    curr_size = 1.0
+    curr_color = ((115.44921875, 206.8046875, 124.484375), '#73ce7c')
+    # list with circles
+    circles = []
+    SizeAreaX = (-100,100)
+    SizeAreaY = (-100,100)
+
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+
         label = tk.Label(self, text="Main Page!", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
-
-        # list with circles
-        self.circles = []
 
         nb = ttk.Notebook(self)
 
         # adding Frames as pages for the ttk.Notebook
         # first page, which would get widgets gridded into it
-        page1 = ttk.Frame(nb)
-
-        # second page
         page2 = ttk.Frame(nb)
 
-        nb.add(page1, text='Model')
+        # second page
+        page1 = ttk.Frame(nb)
+
         nb.add(page2, text='Edit')
+        nb.add(page1, text='Model')
 
         nb.pack(expand=1, fill="both")
 
@@ -76,17 +95,16 @@ class GraphPage(tk.Frame):
         self.ax = self.f.add_subplot(111)
 
 
-        self.ax.set_xlim(-100, 100)
-        self.ax.set_ylim(-100, 100)
+        self.ax.set_xlim(self.SizeAreaX)
+        self.ax.set_ylim(self.SizeAreaY)
+
+        entry['SizeAreaX'] = self.SizeAreaX
+        entry['SizeAreaY'] = self.SizeAreaY
+        entry['SizeSlider'] = self.curr_size
+        entry['Objects'] = self.circles
+        entry['Color'] = self.curr_color
 
 
-        canvas = FigureCanvasTkAgg(self.f, page1)
-        canvas.show()
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
-        toolbar = NavigationToolbar2TkAgg(canvas, page1)
-        toolbar.update()
 
         def IncreaseGraph():
             y_lim = self.ax.axes.get_ylim()
@@ -94,6 +112,9 @@ class GraphPage(tk.Frame):
 
             self.ax.set_xlim(np.multiply(x_lim, 1.5))
             self.ax.set_ylim(np.multiply(y_lim, 1.5))
+
+            entry['SizeAreaX'] = np.multiply(x_lim, 1.5)
+            entry['SizeAreaY'] = np.multiply(y_lim, 1.5)
 
             canvas_edit.draw()
 
@@ -104,7 +125,13 @@ class GraphPage(tk.Frame):
             self.ax.set_xlim(np.divide(x_lim, 1.5))
             self.ax.set_ylim(np.divide(y_lim, 1.5))
 
+            entry['SizeAreaX'] = np.divide(x_lim, 1.5)
+            entry['SizeAreaY'] = np.divide(y_lim, 1.5)
+
             canvas_edit.draw()
+
+        buttonOpen = ttk.Button(page2,text="Open", command=self.file_open)
+        buttonOpen.pack()
 
         buttonPlus = ttk.Button(page2, text="-",
                              command=IncreaseGraph)
@@ -114,6 +141,7 @@ class GraphPage(tk.Frame):
                                 command=DecreaseGraph)
 
         buttonMinus.pack()
+
 
         self.curr_x = 0
         self.curr_y = 0
@@ -126,13 +154,12 @@ class GraphPage(tk.Frame):
 
         self.buttonColor = ttk.Button(page2,text='Select Color', command=self.getColor)
         self.buttonColor.pack()
-        self.curr_color = ((115.44921875, 206.8046875, 124.484375), '#73ce7c')
+
 
         self.v = tk.StringVar(page2, value='1.0')
         self.v.trace("w", lambda name, index, mode, v=self.v: self.ChangeText(v))
         self.labelSlider = tk.Entry(page2,textvariable= self.v)
         self.labelSlider.pack()
-        self.curr_size = 1.0
         self.Slider = ttk.Scale(page2, from_=1.0, to=100.0,command=self.ChangeSize)
         self.Slider.pack()
 
@@ -149,6 +176,35 @@ class GraphPage(tk.Frame):
         self.f.canvas.mpl_connect('motion_notify_event', self.changeCoords)
         self.f.canvas.mpl_connect('button_press_event', self.drawCircle)
 
+    def file_open(self):
+        filename = filedialog.askopenfilename()
+        if filename == "":
+            return
+        if filename is None:  # return `None` if dialog closed with "cancel".
+            return
+        with open(filename, 'rb') as file:
+            entry = pickle.load(file)
+
+        self.ax.set_xlim(entry['SizeAreaX'])
+        self.ax.set_ylim(entry['SizeAreaY'])
+
+        self.labelSlider.delete(0, tk.END)
+        self.labelSlider.insert(0, str(entry['SizeSlider']))
+
+        self.Slider.set(entry['SizeSlider'])
+        self.curr_color = entry['Color']
+        self.SizeAreaY = entry['SizeAreaY'];
+        self.SizeAreaX = entry['SizeAreaX'];
+        self.curr_size = entry['SizeSlider'];
+
+        self.ax.axes.clear()
+        self.circles.clear()
+
+        for elem in entry['Objects']:
+            circle = customCircle(elem.x, elem.y, elem.size, elem.color)
+            self.ax.axes.add_artist(circle)
+            self.circles.append(circle)
+        self.f.canvas.draw()
 
     def changeCoords(self, event):
         if (event.inaxes):
@@ -162,8 +218,10 @@ class GraphPage(tk.Frame):
         if isConvertibleToFloat(v.get()):
             val = float(v.get())
             self.Slider.set(val)
+
     def ChangeSize(self,event):
             self.curr_size = self.Slider.get()
+            entry['SizeSlider'] = self.curr_size
             self.labelSlider.delete(0, tk.END)
             self.labelSlider.insert(0, str(self.curr_size))
 
@@ -173,24 +231,37 @@ class GraphPage(tk.Frame):
             circle = customCircle(self.curr_x, self.curr_y, self.curr_size, self.curr_color[1])
             self.ax.axes.add_artist(circle)
             self.circles.append(circle)
+            entry['Objects'] = self.circles
             self.f.canvas.draw()
 
     def getColor(self):
         self.curr_color = askcolor()
+        entry['Color'] = self.curr_color
 
-    def file_save(self):
-        file = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
-        if file is None:  # asksaveasfile return `None` if dialog closed with "cancel".
-            return
-        text2save = str(self.circles.get(1.0, tk.END))  # starts from `1.0`, not `0.0`
-        file.write(text2save)
-        file.close()  # `()` was missing.
+
 
 class customCircle(Circle):
     def __init__(self, x, y, size, color):
         Circle.__init__(self, (x, y), size)
+        self.x = x
+        self.y = y
+        self.size = size
+        self.color = color
         self.set_color(color)
 
 
+
+
+
 app = SeaofBTCapp()
+menubar = Menu(app)
+text = Text(app)
+text.pack()
+filemenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="File", menu=filemenu)
+
+filemenu.add_command(label="Save", command=file_save)
+
+
+app.config(menu=menubar)
 app.mainloop()
